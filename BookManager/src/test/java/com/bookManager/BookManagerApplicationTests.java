d860999432f88f433b18f79ef61b9e00d997294e
acmors <marcosantoniosilva047@gmail.com>
2024-08-14 21:39:05 -0300

    first commt

---
+++ BookManager/src/test/java/com/bookManager/BookManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
