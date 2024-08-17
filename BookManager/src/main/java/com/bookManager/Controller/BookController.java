e046d3e53fff264ad24adebcc3e556146a4f51aa
acmors <marcosantoniosilva047@gmail.com>
2024-08-16 21:15:59 -0300

    implementacao controller

--- BookManager/src/main/java/com/bookManager/Controller/BookController.java
+++ BookManager/src/main/java/com/bookManager/Controller/BookController.java
@@ -1,13 +1,18 @@
 package com.bookManager.Controller;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.bookManager.entity.Book;
 import com.bookManager.service.BookService;
 
@@ -21,7 +26,7 @@ public class BookController {
 		this.bookService = bookService;
 	}
 	
-	@PostMapping
+	@PostMapping 
 	public ResponseEntity<Book> createBook(@RequestBody Book book){
 		Book newBook = bookService.save(book);
 		return new ResponseEntity<>(newBook, HttpStatus.CREATED);
@@ -33,5 +38,23 @@ public class BookController {
 		return new ResponseEntity<>(books, HttpStatus.OK);
 	}
 	
+	@GetMapping("/{id}")
+	public ResponseEntity<Book> getBookById(@PathVariable Long id){
+		Optional<Book> book = bookService.getBootById(id);
+		return book.map(value -> new ResponseEntity<>(value, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Book> update(@PathVariable Long id, @RequestBody Book bookDatails){
+		Optional<Book> update = bookService.update(id, bookDatails);
+		return update.map(value -> new ResponseEntity<>(value, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		boolean delete = bookService.delete(id);
+		return delete ? new ResponseEntity<>(HttpStatus.NO_CONTENT) : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		
+	}
 	
 }
